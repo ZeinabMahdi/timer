f767baaab13ab65534926f21f6bd18ee6ca6acb1
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-11-30 15:59:29 -0500

    runs but beep doesn't work

--- timer1.js
+++ timer1.js
@@ -1,21 +1,20 @@
 const args = process.argv;
 let timePassed = args.slice(2).sort();
-timePassed = timePassed.filter(args => args > 0);
-timePassed = timePassed.filter(args = Number);
-console.log(timePassed);
+timePassed = timePassed.filter(args => args > 0).filter(args => !isNaN(args));
 
-// const setup = () => {
-// for (let i of timePassed){
-//   if (timePassed.length <= 0) {
-//     break;
-//   } else {
-//   setTimeout(() => {
-//     process.stdout.write('beep' + i);
-// }, i * 1000);
-//   }
-// } 
-// };
 
-// setup();
+const setup = () => {
+for (let i of timePassed){
+  if (timePassed.length <= 0) {
+    break;
+  } else {
+  setTimeout(() => {
+    process.stdout.write('beep at this many seconds: ' + i);
+}, i * 1000);
+  }
+} 
+};
+
+setup();
 
 
